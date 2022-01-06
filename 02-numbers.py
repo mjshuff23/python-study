b447d9d8df0a0b663facbb94e40d87755b5b203a
Michael Shuff <mjshuff23@gmail.com>
2022-01-06 17:58:50 -0500

    test

--- 02-numbers.py
+++ 02-numbers.py
@@ -1,27 +1,3 @@
-# Single Line Comments
-
-# Multi-Line Comments / Documentation / Help
-"""Summary Line
-
-Description
-"""
-
-print('hi\n') # Printing
-num = 30000 # Declaration and Initialization of a variable
-
-def my_function(): # Notice in Python, we use underscores instead of camelCase
-    """My Function Documentation
-    
-    Keyword arguments:
-    argument -- description
-    Return: return_description
-    """
-    print('test')
-
-# How to see multi-line documentation
-help(my_function)
-
-my_function()
 
 ###### NUMBERS ################################################################
 # Python has three numeric types
