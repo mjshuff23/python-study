acb8074c644897983481d570b7b7e1dc92a148a8
Michael Shuff <mjshuff23@gmail.com>
2022-01-06 17:44:52 -0500

    finished strings

--- 03-strings.py
+++ 03-strings.py
@@ -29,3 +29,35 @@ print("Banana".count('na'))
 # concatenation - Python uses the `+` to concatenate
 #               - Multiply a string with `*`
 print("hi"*5)
+
+# format()
+#   - First, use placeholders in the string where you want the data to go. A 
+#      placeholder is a number inside braces like this {0}. Start at zero and 
+#      each placeholder gets the next number. The use the format function on
+#      your string and pass in the data to substitute.
+first_name = "Michael"
+last_name = "Shuff"
+print('Your name is {0} {1}'.format(first_name, last_name))
+# For simple uses, a fast way to apply formatting is using the 'f' flag on the 
+#  string. This means putting using single quotes with an f at the start.
+print(f'Your name is {first_name} {last_name}')
+
+# Other Useful Methods
+print(first_name.upper()) # MIKE
+print(first_name.lower()) # mike
+print(first_name.isupper()) # false
+print(first_name.islower()) # false
+print(first_name.startswith('Mi')) # true
+print(first_name.endswith('ael')) # true
+full_name = "Michael Shuff"
+print(full_name.split()) # ['Michael', 'Shuff']
+print(full_name.split('h')) # ['Mic', 'ael S', 'uff']
+names_array = ['Michael', 'Shuff']
+print(' '.join(names_array)) # join() is on the string instead of the array
+
+## Even More Methods
+print('Mike'.isalpha()) # True if string consists of letters and is not blank
+print('Mi234234'.isalnum()) # True is string consists of letters and numbers and is not blanks
+print('23523523'.isdecimal()) # True is only numeric characters and not blank
+print('    '.isspace()) # True if consists of only spaces, tabs, and new lines
+print('Mike'.istitle()) # True if it is only words that begin capitalized
