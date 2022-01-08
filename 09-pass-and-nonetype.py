80479be8c98f977154c0c47575932d7d482d720f
Michael Shuff <mjshuff23@gmail.com>
2022-01-08 18:35:30 -0500

    added 09-pass-and-nonetype

---
+++ 09-pass-and-nonetype.py
@@ -0,0 +1,24 @@
+#### PASS / NONETYPE ###########################################################
+
+### Pass #######################################################################
+# - When declaring an empty code block, whether it is a def, for, while, or if,
+#    you want to add `pass` to the body of the code to avoid an error
+if True:
+    pass
+
+while True:
+    pass
+################################################################################
+
+### NoneType Data Type #########################################################
+# - Where JavaScript has both the null and undefined values that can imply the 
+#    lack of existence of a value, Python only has one.
+#
+# - In Python there is a value called `None`, which represents the absence of a
+#    value. `None` is the only value of the `NoneType` data type. Just like the 
+#    Boolean `True` and `False` values, `None` must be typed with a capital N.
+#
+# - One place where None is used is as the return value of functions that do 
+#    not explicitly return a value, much in the same way that JavaScript will 
+#    return undefined for functions that do not return a value.
+################################################################################
