c132dca0674f36c8087bad0cea6580e82202f05a
Michael Shuff <mjshuff23@gmail.com>
2022-01-08 18:10:54 -0500

    finished 07-conditionals-and-loops

--- 07-conditionals-and-loops.py
+++ 07-conditionals-and-loops.py
@@ -19,3 +19,30 @@ elif name == "Sofia":
     print("Hey, Sofia")
 else:
     print("Not John OR Sofia")
+
+
+### While Statements ######
+spam = 0
+
+while spam < 5:
+    print('Hello, World')
+    spam+=1
+
+# Breaking out early
+spam = 0
+
+while True:
+    print("Hello again, World")
+    spam+=1
+    if spam >= 5:
+        break
+
+# Continuing through an iteration
+spam = 0
+
+while spam <= 10:
+    if spam % 2 == 1:
+        spam+=1
+        continue
+    print(spam)
+    spam+=1
