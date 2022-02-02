b57992a01edca60276d262c1c8e9b39c537658f8
Michael Shuff <mjshuff23@gmail.com>
2022-02-02 13:39:44 -0500

    finished files.py

--- Built-In-Methods/files.py
+++ Built-In-Methods/files.py
@@ -1,9 +1,28 @@
 # open() - returns a file object, which has a read() method
-# open(file, mode='r', buffering=-1, encoding=None, errors=None, newline=None,
-#      closefd=True, opener=None)
+# open(file, mode='r') - opens a file in read mode
 #  - file: The file to be opened.
 #  - mode: The mode to open the file:
 #     - 'r' for read, 'w' for write, 'a' for append.
 
 read_stream = open("test.txt", "r")
-print(read_stream.read())
+
+# read() - reads the entire file
+# read(chars) - reads the specified number of chars
+# print(read_stream.read())
+
+# readline() - reads the next line
+# readline(chars) - reads the specified number of chars
+print(read_stream.readline())
+
+# readlines() - reads the entire file and returns a list of lines
+print(read_stream.readlines())
+
+# close() - closes the file
+# read_stream.close()
+# read_stream.close()
+
+# flush() - flushes the write buffer
+# read_stream.flush()
+
+# readable() - returns True if the file is open for reading
+print(read_stream.readable())
